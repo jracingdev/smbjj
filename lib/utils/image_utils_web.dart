89d1b0fec982d25ed990e420614ea8dcff5692c3
JRacing <jracingdev@gmail.com>
2026-06-03 17:25:36 -0300

    Fix web: local paths ignorados + gft_logo incluida no build

--- lib/utils/image_utils_web.dart
+++ lib/utils/image_utils_web.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
-/// Web: todos os caminhos tratados como URL (blob: ou http:)
-ImageProvider imageProviderFromPath(String path) => NetworkImage(path);
+bool _isValidUrl(String path) =>
+    path.startsWith('http://') ||
+    path.startsWith('https://') ||
+    path.startsWith('blob:');
 
-/// Web: usa Image.network para tudo (blob: URLs funcionam)
+/// Web: só URLs válidas funcionam (http/https/blob)
+/// Paths locais do Android (/data/user/...) são ignorados
+ImageProvider imageProviderFromPath(String path) {
+  if (!_isValidUrl(path)) return const AssetImage('assets/images/logo.png');
+  return NetworkImage(path);
+}
+
 Widget imageWidgetFromPath(String path, {BoxFit fit = BoxFit.cover, Widget? errorWidget}) {
+  if (!_isValidUrl(path)) return errorWidget ?? const SizedBox();
   return Image.network(path, fit: fit,
       errorBuilder: (_, __, ___) => errorWidget ?? const SizedBox());
 }
